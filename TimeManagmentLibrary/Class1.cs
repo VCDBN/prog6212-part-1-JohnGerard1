42d68a27d0ef3f468f232bf23a1fc4ddff6dbc8d
JohnGerard1 <124500120+JohnGerard1@users.noreply.github.com>
2023-09-22 19:12:02 +0200

    Add files via upload

---
+++ TimeManagmentLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace TimeManagmentLibrary
+{
+    public class Module
+    {
+        //code attribution - GeeksForGeeks
+        // Properties of the Module class.
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public int ClassHours { get; set; }
+        public double SelfStudyHours { get; set; }
+        public double HoursSpent { get; set; }
+        public double RemainingHours { get; set; }
+
+        // Collection to hold recorded hours for each module.
+        public ObservableCollection<RecordedHour> RecordedHours { get; set; } = new ObservableCollection<RecordedHour>();
+    }
+
+    public class RecordedHour
+    {
+        public DateTime Date { get; set; }
+        public double Hours { get; set; }
+    }
+}
